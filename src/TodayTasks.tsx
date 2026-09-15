6946df2f252c7e8c800a6acdde89427219882d48
Rintaroman <rintaro.584@gmail.com>
2026-09-15 23:49:55 +0900

    タスクの追加機能実装

--- src/TodayTasks.tsx
+++ src/TodayTasks.tsx
@@ -1,31 +1,52 @@
 import { useState } from 'react'
 
-function TodaysTasks() {
+function TodayTasks() {
   const [tasks, setTasks] = useState([
-    {id: 1,title: 'ゴミを出す', completed: false},
-    {id: 2,title: '買い物に行く', completed: true},
-    {id: 3,title: '子供を寝かせる', completed: false},
+    { id: 1, title: 'ゴミを出す', completed: false },
+    { id: 2, title: '買い物に行く', completed: true },
+    { id: 3, title: '子どもを寝かせる', completed: false },
   ])
+
+  const [newTaskTaitle, setNewTaskTaitle] = useState('')
 
   const toggleTask = (id: number) => {
     setTasks(
-      tasks.map((task) => task.id === id
-    ? {...task, completed: !task.completed}
-    :task
+      tasks.map((task) =>
+        task.id === id
+          ? { ...task, completed: !task.completed }
+          : task
       )
     )
   }
 
-    return(
-      <section>
-        <h2>今日やること</h2>
-        {tasks.map((task) => (
-          <button onClick={() => toggleTask(task.id)}>
-            {task.completed ? '☑' : '☐'} {task.title}
-          </button>
-        ))}
-      </section>
-    )
+  const addTask = () => {
+    const newTask ={
+      id: tasks.length +1,
+      title: newTaskTaitle,
+      completed: false,
+    }
+
+    setTasks([...tasks, newTask])
   }
 
-  export default TodaysTasks
+  return (
+    <section>
+      <h2>今日やること</h2>
+      <input 
+      type="text"
+      value ={newTaskTaitle}
+      onChange={(e) => setNewTaskTaitle(e.target.value)}
+      />
+      <button onClick={() => console.log(newTaskTaitle)}>追加</button>
+
+
+      {tasks.map((task) => (
+        <button key={task.id} onClick={() => toggleTask(task.id)}>
+          {task.completed ? '☑' : '☐'} {task.title}
+        </button>
+      ))}
+    </section>
+  )
+}
+
+export default TodayTasks
